ce407802987c7897894211cf7928770e4dc36122
DOSarrest <dev@dosarrest.com>
2016-11-21 21:20:13 -0800

    TLS Rework
    Renaming to AutoclosingLdapContext and making it extend InitialLdapContext instead of InitialDirContext

---
+++ src/main/java/com/yammer/dropwizard/authenticator/AutoclosingLdapContext.java
@@ -0,0 +1,48 @@
+package com.yammer.dropwizard.authenticator;
+
+import javax.naming.NamingException;
+import javax.naming.ldap.InitialLdapContext;
+import javax.naming.ldap.StartTlsRequest;
+import javax.naming.ldap.StartTlsResponse;
+
+import java.io.IOException;
+import java.util.Hashtable;
+
+public class AutoclosingLdapContext extends InitialLdapContext implements AutoCloseable {
+    private StartTlsResponse tls;
+
+	public AutoclosingLdapContext() throws NamingException {
+        super();
+    }
+
+    public AutoclosingLdapContext(Hashtable<?, ?> environment, Boolean tls) throws NamingException {
+        super(environment, null);
+        if (tls==true) {
+        	loadTLS();
+        }
+    }
+    
+    public void loadTLS() {
+    	try {
+			tls = (StartTlsResponse) this.extendedOperation(new StartTlsRequest());
+			tls.negotiate();
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+
+    @Override
+    public void close() throws NamingException {
+    	try {
+			tls.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        super.close();
+    }
+}
